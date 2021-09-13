3999a0c223e7f0d546a45858daadaac9e9b2d614
Lucasino20 <brhandon.gutierrez@utec.edu.pe>
2021-09-13 03:46:34 -0500

    fds

--- circulararray.h
+++ circulararray.h
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
+/*template< class T>
+void resize(T *array ,int new_size){
+    int *array2 = new int[new_size];
+    for(int i = 0 ; i < size(); i++)
+        array2[i] = array[i];
+    delete[] array;
+    array = array2;
+}*/
+
 template <class T>
 class CircularArray
 {
-private:
+protected:
     T *array;
     int capacity;
     int back, front;
@@ -28,7 +37,7 @@ public:
     void reverse();
     string to_string(string sep=" ");
 
-private:
+protected:
     int next(int);
     int prev(int);
 };
@@ -73,3 +82,106 @@ string CircularArray<T>::to_string(string sep)
         result += std::to_string((*this)[i]) + sep;
     return result;    
 }
+
+template<class T>
+void CircularArray<T>::push_front(T data){
+    if( array == nullptr){
+        array = new int[1];
+        array[1] = data;
+    }
+    else if(size() == capacity){
+        int newsize = 0;
+        newsize = capacity + 1;
+        //resize(array , newsize);
+        array[newsize] = data;
+
+        }
+}
+
+template <class T>
+void CircularArray<T>::push_back(T data ){
+        if( array == nullptr){
+        array = new int[1];
+        array[1] = data;
+    }
+    else if(size() == capacity){
+        int newsize = 0;
+        newsize = capacity + 1;
+        int *array2 = new int[newsize];
+        for(int i = 0 ; i < size(); i++)
+            array2[i] = array[i];
+        delete[] array;
+        array = array2;
+        array[newsize] = data;
+        this->back = array[newsize];
+
+        }
+
+
+}
+
+template <class T>
+void CircularArray<T>::insert(T data , int pos){
+
+}
+
+template <class T>
+T CircularArray<T>::pop_front(){
+
+}
+
+
+template<class T>
+T CircularArray<T>::pop_back() {
+    if(array == nullptr || size() == 0){
+        throw ("empty");
+    }
+
+}
+
+template < class T>
+bool CircularArray<T>::is_full(){
+    return next(back) == front;
+}
+
+template <class T>
+bool CircularArray<T>::is_empty(){
+    return (front == back || array == nullptr);
+}
+
+
+template<class T>
+int CircularArray<T>::size(){
+    return size_t(array);
+
+}
+
+template< class T>
+void CircularArray<T>::clear(){
+    delete[] array;
+
+}
+
+template<class T>
+T  &CircularArray<T>::operator[](int){
+    
+}
+
+template < class T>
+void CircularArray<T>::sort(){
+
+}
+
+template < class T>
+bool CircularArray<T>::is_sorted(){
+    return true;
+}
+
+
+template < class T>
+void CircularArray<T>::reverse(){
+
+}
+
+
+
